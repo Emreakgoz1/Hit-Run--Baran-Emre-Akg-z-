27a7fbbe833c3d63436d450d1aafc93490dfc0d6
Emre Akgöz <baranemreakgoz@gmail.com>
2022-05-24 20:14:21 +0300

    Bullet hit with tag

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -4,22 +4,37 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
-        [SerializeField] GameObject Bullet_Emitter;
-        [SerializeField] GameObject Bullet;
-        [SerializeField] float Bullet_Forward_Force;
-      
-        void Update()
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                GameObject Temporary_Bullet_Handler;
-                Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
-                Temporary_Bullet_Handler.transform.Rotate(90,0,0);
-                Rigidbody Temporary_RigidBody;
-                Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
-                Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force);
-                Destroy(Temporary_Bullet_Handler, 5.0f);
-            }
+    [SerializeField] GameObject Bullet_Emitter;
+    [SerializeField] GameObject Bullet;
+    [SerializeField] float Bullet_Forward_Force;
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward) , out hit,Mathf.Infinity) && hit.transform.tag == "Destroyable")
+        {   
+            Debug.DrawRay(transform.position,transform.TransformDirection(Vector3.forward)*hit.distance,Color.red);
+       
+            ÝnstantiateBulelts();
         }
-    
+    }
+    public void ÝnstantiateBulelts()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+
+            GameObject Temporary_Bullet_Handler;
+            Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+            Temporary_Bullet_Handler.transform.Rotate(90, 0, 0);
+            Rigidbody Temporary_RigidBody;
+            Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+            Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force);
+            Destroy(Temporary_Bullet_Handler, 5.0f);
+        }
+    }
 }
